604e51a612a9d01767f962dbf581e53b17010cc4
Alfian Losari <alfianlosari@gmail.com>
2020-03-16 23:33:05 +0800

    Initial Commit

---
+++ ImageFilterSwiftUI/Bindings/ImageFilterObervable.swift
@@ -0,0 +1,29 @@
+//
+//  ImageFilterObervable.swift
+//  ImageFilterMac
+//
+//  Created by Alfian Losari on 25/02/20.
+//  Copyright © 2020 Alfian Losari. All rights reserved.
+//
+
+import Combine
+import AppKit
+
+class ImageFilterObservable: ObservableObject {
+    
+    @Published var filteredImage: NSImage? = nil
+
+    let image: NSImage
+    let filter: ImageFilter
+    
+    init(image: NSImage, filter: ImageFilter) {
+        self.image = image
+        self.filter = filter
+    }
+    
+    func filterImage() {
+        self.filter.performFilter(with: self.image) {
+            self.filteredImage = $0
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,15 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ImageFilterSwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for ImageFilterSwiftUI
+  pod 'MetalPetal'
+
+end
+
+
+
+
